59e7d03513ff15bbe43c91a09268363dbf803ff3
simon cheche <chechesimon51@gmail.com>
2024-11-08 19:18:45 +0200

    .

--- gift_countdown/gift_countdown/settings.py
+++ gift_countdown/gift_countdown/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4)!zc*zcbcx4kf2doaff7x5ff*!r!lahnc_dh5x)1#-u!p%!tg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['main.up.railway.app']
+ALLOWED_HOSTS = ['127.0.0.1', 'main.up.railway.app', 'https://mango-szn.onrender.com']
 
 
 # Application definition
